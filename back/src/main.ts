8fb2c8f8ed76b9820a1033b06bdfc227eced8f3e
Tolomatico <ballesty.t@gmail.com>
2025-09-01 03:37:38 -0300

    fix

--- back/src/main.ts
+++ back/src/main.ts
@@ -16,7 +16,7 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(port, '0.0.0.0');
+  await app.listen(port);
   console.log(`🚀 Application is running on: http://localhost:${port}`);
 }
 bootstrap();
