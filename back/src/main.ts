f288a0d352c82fa92126615ce4cc9322e0ee6756
Tolomatico <ballesty.t@gmail.com>
2025-08-31 23:22:22 -0300

    Added port

--- back/src/main.ts
+++ back/src/main.ts
@@ -3,10 +3,11 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 
-const port = process.env.PORT || 3001
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT || 3001
   app.enableCors();
   app.useGlobalPipes(
     new ValidationPipe({
